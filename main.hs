7ff28ccb61e22688228485d70e17644947e1d14e
Eric Puentes <eric.david2333@gmail.com>
2023-09-13 23:20:38 +0200

    do syntax sugar

--- main.hs
+++ main.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE LambdaCase #-}
-
+import Control.Monad
 import Convert
 import Html
 import Markup
@@ -7,34 +6,39 @@ import System.Directory (doesFileExist)
 import System.Environment (getArgs)
 
 main :: IO ()
-main =
-  getArgs >>= \case
-    [] -> getContents >>= \content -> putStrLn (process "User input" content) -- TODO: Too lazy to work
-    [input, output] ->
-      doesFileExist output >>= \case
-        True ->
-          confirm output >>= \case
-            True -> readAndWrite input output
-            False -> putStr "Bye then."
-        False -> readAndWrite input output
+main = do
+  args <- getArgs
+  case args of
+    [] -> do
+      content <- getContents
+      putStrLn (process "User input" content) -- TODO: Too lazy to work
+    [input, output] -> do
+      content <- readFile input
+      exist <- doesFileExist output
+      let writeResult = writeFile output $ process input content
+      if exist
+        then whenIO (confirm input) writeResult
+        else writeResult
     _ ->
       putStr
         "Incorrect use!.\nValid uses:\n1)call the program without arguments\
         \to parse the stdin.\n2) call the program with two arguments the input file and the outputfile"
 
-readAndWrite :: FilePath -> FilePath -> IO ()
-readAndWrite input output = readFile input >>= \content -> writeFile output $ process input content
+whenIO :: IO Bool -> IO () -> IO ()
+whenIO cond action = do
+  result <- cond
+  when result action
 
 confirm :: String -> IO Bool
-confirm name =
+confirm name = do
   putStrLn ("Do you want to overwrite the file " <> name <> "? (y/n)")
-    *> getLine
-    >>= \case
-      "y" -> pure True
-      "n" -> pure False
-      _ ->
-        putStrLn "Invalid response. use y or n"
-          *> confirm name
+  line <- getLine
+  case line of
+    "y" -> pure True
+    "n" -> pure False
+    _ -> do
+      putStrLn "Invalid response. use y or n"
+      confirm name
 
 process :: Html.Title -> String -> String
 process title = Html.render . convert title . Markup.parse
